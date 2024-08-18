68106e4ed03e8a9dc8b58a2ea6baa8bcee8a8089
mirohid <mirohid7860@gmail.com>
2024-08-18 23:00:01 +0530

    Initial Commit

---
+++ E-commerce-Furniture/E_commerce_FurnitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_commerce_FurnitureApp.swift
+//  E-commerce-Furniture
+//
+//  Created by Mir Ohid Ali on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct E_commerce_FurnitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
